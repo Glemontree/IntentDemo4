7d7bef17d3a819342076fb7c6f87fa04239c7942
gtp <gtp0521@gmail.com>
2016-08-25 20:00:12 +0800

    first commit

---
+++ app/src/main/java/com/glemontree/intentdemo4/SecondActivity.java
@@ -0,0 +1,30 @@
+package com.glemontree.intentdemo4;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+/**
+ * Created by Administrator on 2016/8/25.
+ */
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.second_layout);
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("data_return", "Hello MainActivity");
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+}
